028820812054e74ffea52015a67a73d87013284a
meinertvladyslav <vladyslavmeinert@gmail.com>
2025-12-12 12:36:21 +0000

    Did some work for MainPage.xaml, so I could in the future make working method to reach through movies, and add some comments and methods

--- Cross-Platform-Project/MainPage.xaml.cs
+++ Cross-Platform-Project/MainPage.xaml.cs
@@ -14,6 +14,7 @@ namespace Cross_Platform_Project
         //Local cache will be saved here
         string localCache = Path.Combine(FileSystem.AppDataDirectory, "movieList.json");
         string json;
+        string text;
         //creating variable movie list with Movies class inside
         List<Movies> movieList = new List<Movies>();
         //helps with downloading file
@@ -29,33 +30,43 @@ namespace Cross_Platform_Project
 
                     //created string json using server client to get response from website(url) in string format
                     json = await client.GetStringAsync(url);
-
+                    //writing everything here
                     File.WriteAllText(localCache, json);
 
 
                 }
                 else
                 {
+                    //stores everything here
                     json = File.ReadAllText(localCache);
                 }
             }
-
+            //if something happend it will let me know
             catch (Exception ex)
             {
                 Console.WriteLine("ERROR, Problem detected", ex.Message);
             }
         }
+        //method that helps me with searching
         private void Looking(object sender, TextChangedEventArgs e)
+        {
+            //making everything uppercase, so its wouldnt be case sensetive
+            text = e.NewTextValue.ToUpper();
+            if (String.IsNullOrEmpty(text))
+            {
+
+            }
+        }
+        private void ListOfMovies_Tapped(object sender, EventArgs e)
         {
 
         }
 
 
-
         public MainPage()
         {
             InitializeComponent();
-
+            
         }
 
        
